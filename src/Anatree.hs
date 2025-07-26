6a1b1b44cfb75d55be33dca7e1b48da2e6626489
Steffan Sølvsten <soelvsten@cs.au.dk>
2025-07-26 12:58:14 +0200

    Add 'insert' and 'member' functions (WIP)

--- src/Anatree.hs
+++ src/Anatree.hs
@@ -1,15 +1,43 @@
 module Anatree where
+import qualified Data.Set as Set
+import Data.List (sort)
 
-data Tree = Leaf
+data Tree = Leaf (Set.Set [Char])
+          | Node (Set.Set [Char]) Char (Tree) (Tree)
 
 -- |Creates an empty anagram tree.
-empty :: () -> Tree
-empty () = Leaf
+empty :: Tree
+empty = Leaf (Set.empty)
 
--- |Number of words stored within anagram tree.
+-- |Add word `w` to anagram tree.
+insert :: [Char] -> Tree -> Tree
+insert w t = insert' (sort w) t
+  where insert' []     (Leaf ws)         = Leaf (Set.insert w ws)
+        insert' []     (Node ws c t0 t1) = Node (Set.insert w ws) c t0 t1
+        insert' (x:xs) (Leaf ws)         = Node Set.empty x (Leaf ws) (insert' xs empty)
+        insert' (x:xs) t' = let (Node ws c t0 t1) = t'
+          in if x < c  then Node Set.empty x t' (insert' xs empty) else
+             if x == c then Node ws        c t0 (insert' xs t1)
+                       else Node ws        c (insert' (x:xs) t0) t1
+
+-- |Whether the word `w` is in the anagram tree.
+member :: [Char] -> Tree -> Bool
+member w t = member' (sort w) t
+  where member' []     (Leaf ws)         = Set.member w ws
+        member' []     (Node ws _  _ _)  = Set.member w ws
+        member' _      (Leaf _)          = False
+        member' (x:xs) t' = let (Node _ c t0 t1) = t'
+          in if x < c  then member' xs     t' else
+             if x == c then member' xs     t1
+                       else member' (x:xs) t0
+
+-- |Number of ws stored within anagram tree.
 size :: Tree -> Int
-size _ = 0
+size (Leaf ws) = Set.size ws
+size (Node ws _ t0 t1) = (Set.size ws) + (size t0) + (size t1)
 
 -- |Size of the anagram tree itself, not the words within.
 treeSize :: Tree -> Int
-treeSize _ = 1
+treeSize (Leaf _) = 1
+treeSize (Node _ _ t0 t1) = 1 + (treeSize t0) + (treeSize t1)
+
--- test/Main.hs
+++ test/Main.hs
@@ -4,16 +4,194 @@ import Test.Framework.Providers.HUnit
 import Test.HUnit
 import qualified Anatree as Anatree
 
-test_emptyInfo :: Test.HUnit.Test
-test_emptyInfo = let t = Anatree.empty ()
-                 in Test.HUnit.TestList [
-  "Empty tree has no words" ~: (Anatree.size t) ~=? 0,
-  "Empty tree has size 1" ~: (Anatree.treeSize t) ~=? 1
+-- Tests with the empty tree [2]
+tests_Empty :: Test.HUnit.Test
+tests_Empty = let t = Anatree.empty
+  in "{}" ~: Test.HUnit.TestList [
+  "Has size 0"            ~: 0     ~=? (Anatree.size t),
+  "Has a tree of size 1"  ~: 1     ~=? (Anatree.treeSize t),
+  "Does not contain ''"   ~: False ~=? (Anatree.member "" t),
+  "Does not contain 'a'"  ~: False ~=? (Anatree.member "a" t),
+  "Does not contain 'b'"  ~: False ~=? (Anatree.member "b" t),
+  "Does not contain 'ab'" ~: False ~=? (Anatree.member "ab" t),
+  "Does not contain 'ba'" ~: False ~=? (Anatree.member "ba" t)
+  ]
+
+tests_Eta :: Test.HUnit.Test
+tests_Eta = let t = Anatree.insert "" $ Anatree.empty
+  in "{''}" ~: Test.HUnit.TestList [
+  "Has size 1"            ~: 1     ~=? (Anatree.size t),
+  "Has a tree of size 1"  ~: 1     ~=? (Anatree.treeSize t),
+  "Contains ''"           ~: True  ~=? (Anatree.member "" t),
+  "Does not contain 'a'"  ~: False ~=? (Anatree.member "a" t),
+  "Does not contain 'b'"  ~: False ~=? (Anatree.member "b" t),
+  "Does not contain 'ab'" ~: False ~=? (Anatree.member "ab" t),
+  "Does not contain 'ba'" ~: False ~=? (Anatree.member "ba" t)
+  ]
+
+-- Tests with a single 'a' node [2]
+tests_A :: Test.HUnit.Test
+tests_A = let t = Anatree.insert "a" $ Anatree.empty
+  in "{'a'}" ~: Test.HUnit.TestList [
+  "Has size 1"            ~: 1     ~=? (Anatree.size t),
+  "Has a tree of size 3"  ~: 3     ~=? (Anatree.treeSize t),
+  "Does not contains ''"  ~: False ~=? (Anatree.member "" t),
+  "Contains 'a'"          ~: True  ~=? (Anatree.member "a" t),
+  "Does not contain 'b'"  ~: False ~=? (Anatree.member "b" t),
+  "Does not contain 'ab'" ~: False ~=? (Anatree.member "ab" t),
+  "Does not contain 'ba'" ~: False ~=? (Anatree.member "ba" t)
+  ]
+
+tests_A_A :: Test.HUnit.Test
+tests_A_A = let t = Anatree.insert "a" $ Anatree.insert "a" $ Anatree.empty
+  in "{'a'}" ~: Test.HUnit.TestList [
+  "Has size 1"            ~: 1     ~=? (Anatree.size t),
+  "Has a tree of size 3"  ~: 3     ~=? (Anatree.treeSize t),
+  "Does not contains ''"  ~: False ~=? (Anatree.member "" t),
+  "Contains 'a'"          ~: True  ~=? (Anatree.member "a" t),
+  "Does not contain 'b'"  ~: False ~=? (Anatree.member "b" t),
+  "Does not contain 'ab'" ~: False ~=? (Anatree.member "ab" t),
+  "Does not contain 'ba'" ~: False ~=? (Anatree.member "ba" t)
+  ]
+
+-- Tests with a single 'b' node [2]
+tests_B :: Test.HUnit.Test
+tests_B = let t = Anatree.insert "b" Anatree.empty
+  in "{'b'}" ~: Test.HUnit.TestList [
+  "Has size 1"            ~: 1     ~=? (Anatree.size t),
+  "Has a tree of size 3"  ~: 3     ~=? (Anatree.treeSize t),
+  "Does not contains ''"  ~: False ~=? (Anatree.member "" t),
+  "Does not contain 'a'"  ~: False ~=? (Anatree.member "a" t),
+  "Contains 'b'"          ~: True  ~=? (Anatree.member "b" t),
+  "Does not contain 'ab'" ~: False ~=? (Anatree.member "ab" t),
+  "Does not contain 'ba'" ~: False ~=? (Anatree.member "ba" t)
+  ]
+
+-- Tests with an 'a,b' chain [2]
+tests_A_B :: Test.HUnit.Test
+tests_A_B = let t = Anatree.insert "a" $ Anatree.insert "b" $ Anatree.empty
+  in "{'a','b'}" ~: Test.HUnit.TestList [
+  "Has size 2"            ~: 2     ~=? (Anatree.size t),
+  "Has a tree of size 5"  ~: 5     ~=? (Anatree.treeSize t),
+  "Does not contains ''"  ~: False ~=? (Anatree.member "" t),
+  "Contains 'a'"          ~: True  ~=? (Anatree.member "a" t),
+  "Contains 'b'"          ~: True  ~=? (Anatree.member "b" t),
+  "Does not contain 'ab'" ~: False ~=? (Anatree.member "ab" t),
+  "Does not contain 'ba'" ~: False ~=? (Anatree.member "ba" t)
+  ]
+
+tests_B_A :: Test.HUnit.Test
+tests_B_A = let t = Anatree.insert "b" $ Anatree.insert "a" $ Anatree.empty
+  in "{'a','b'}" ~: Test.HUnit.TestList [
+  "Has size 2"            ~: 2     ~=? (Anatree.size t),
+  "Has a tree of size 5"  ~: 5     ~=? (Anatree.treeSize t),
+  "Does not contains ''"  ~: False ~=? (Anatree.member "" t),
+  "Contains 'a'"          ~: True  ~=? (Anatree.member "a" t),
+  "Contains 'b'"          ~: True  ~=? (Anatree.member "b" t),
+  "Does not contain 'ab'" ~: False ~=? (Anatree.member "ab" t),
+  "Does not contain 'ba'" ~: False ~=? (Anatree.member "ba" t)
+  ]
+
+tests_AB_BA :: Test.HUnit.Test
+tests_AB_BA = let t = Anatree.insert "ab" $ Anatree.insert "ba" $ Anatree.empty
+  in "{'ab','ba'}" ~: Test.HUnit.TestList [
+  "Has size 2"            ~: 2     ~=? (Anatree.size t),
+  "Has a tree of size 5"  ~: 5     ~=? (Anatree.treeSize t),
+  "Does not contains ''"  ~: False ~=? (Anatree.member "" t),
+  "Does not contain 'a'"  ~: False ~=? (Anatree.member "a" t),
+  "Does not contain 'b'"  ~: False ~=? (Anatree.member "b" t),
+  "Contains 'ab'"         ~: True  ~=? (Anatree.member "ab" t),
+  "Contains 'ba'"         ~: True  ~=? (Anatree.member "ba" t)
+  ]
+
+tests_BA_AB :: Test.HUnit.Test
+tests_BA_AB = let t = Anatree.insert "ba" $ Anatree.insert "ab" $ Anatree.empty
+  in "{'ba','ab'}" ~: Test.HUnit.TestList [
+  "Has size 2"            ~: 2     ~=? (Anatree.size t),
+  "Has a tree of size 5"  ~: 5     ~=? (Anatree.treeSize t),
+  "Does not contains ''"  ~: False ~=? (Anatree.member "" t),
+  "Does not contain 'a'"  ~: False ~=? (Anatree.member "a" t),
+  "Does not contain 'b'"  ~: False ~=? (Anatree.member "b" t),
+  "Contains 'ab'"         ~: True  ~=? (Anatree.member "ab" t),
+  "Contains 'ba'"         ~: True  ~=? (Anatree.member "ba" t)
+  ]
+
+tests_A_AB_A :: Test.HUnit.Test
+tests_A_AB_A = let t = Anatree.insert "a" $ Anatree.insert "ab"
+                     $ Anatree.insert "a" $ Anatree.empty
+  in "{'a','ab', 'a'}" ~: Test.HUnit.TestList [
+  "Has size 2"            ~: 2     ~=? (Anatree.size t),
+  "Has a tree of size 5"  ~: 5     ~=? (Anatree.treeSize t),
+  "Does not contains ''"  ~: False ~=? (Anatree.member "" t),
+  "Contains 'a'"          ~: True  ~=? (Anatree.member "a" t),
+  "Does not contain 'b'"  ~: False ~=? (Anatree.member "b" t),
+  "Contains 'ab'"         ~: True  ~=? (Anatree.member "ab" t),
+  "Does not contain 'ba'" ~: False ~=? (Anatree.member "ba" t)
+  ]
+
+-- Tests with one 'a' node and two 'b' nodes.
+tests_AB_B :: Test.HUnit.Test
+tests_AB_B = let t = Anatree.insert "ab" $ Anatree.insert "b" $ Anatree.empty
+  in "{'ab','b'}" ~: Test.HUnit.TestList [
+  "Has size 2"            ~: 2     ~=? (Anatree.size t),
+  "Has a tree of size 5"  ~: 7     ~=? (Anatree.treeSize t),
+  "Does not contains ''"  ~: False ~=? (Anatree.member "" t),
+  "Does not contain 'a'"  ~: False ~=? (Anatree.member "a" t),
+  "Contains 'b'"          ~: True  ~=? (Anatree.member "b" t),
+  "Contains 'ab'"         ~: True  ~=? (Anatree.member "ab" t),
+  "Does not contain 'ba'" ~: False ~=? (Anatree.member "ba" t)
+  ]
+
+tests_B_AB_B :: Test.HUnit.Test
+tests_B_AB_B = let t = Anatree.insert "b" $ Anatree.insert "ab"
+                     $ Anatree.insert "b" $ Anatree.empty
+  in "{'b','ab', 'b'}" ~: Test.HUnit.TestList [
+  "Has size 2"            ~: 2     ~=? (Anatree.size t),
+  "Has a tree of size 5"  ~: 7     ~=? (Anatree.treeSize t),
+  "Does not contains ''"  ~: False ~=? (Anatree.member "" t),
+  "Does not contain 'a'"  ~: False ~=? (Anatree.member "a" t),
+  "Contains 'b'"          ~: True  ~=? (Anatree.member "b" t),
+  "Contains 'ab'"         ~: True  ~=? (Anatree.member "ab" t),
+  "Does not contain 'ba'" ~: False ~=? (Anatree.member "ba" t)
+  ]
+
+tests_BA_AB_B :: Test.HUnit.Test
+tests_BA_AB_B = let t = Anatree.insert "ba" $ Anatree.insert "ab"
+                      $ Anatree.insert "b"  $ Anatree.empty
+  in "{'ba','ab', 'b'}" ~: Test.HUnit.TestList [
+  "Has size 3"            ~: 3     ~=? (Anatree.size t),
+  "Has a tree of size 7"  ~: 7     ~=? (Anatree.treeSize t),
+  "Does not contains ''"  ~: False ~=? (Anatree.member "" t),
+  "Does not contain 'a'"  ~: False ~=? (Anatree.member "a" t),
+  "Contains 'b'"          ~: True  ~=? (Anatree.member "b" t),
+  "Contains 'ab'"         ~: True  ~=? (Anatree.member "ab" t),
+  "Contains 'ba'"         ~: True  ~=? (Anatree.member "ba" t)
+  ]
+
+tests_Eta_BA_A_AB_B :: Test.HUnit.Test
+tests_Eta_BA_A_AB_B = let t = Anatree.insert ""  $ Anatree.insert "ba"
+                            $ Anatree.insert "a" $ Anatree.insert "ab"
+                            $ Anatree.insert "b" $ Anatree.empty
+  in "{'', 'ba', 'a', 'ab', 'b'}" ~: Test.HUnit.TestList [
+  "Has size 5"           ~: 5     ~=? (Anatree.size t),
+  "Has a tree of size 7" ~: 7     ~=? (Anatree.treeSize t),
+  "Contains ''"          ~: True  ~=? (Anatree.member "" t),
+  "Contain  'a'"         ~: True  ~=? (Anatree.member "a" t),
+  "Contains 'b'"         ~: True  ~=? (Anatree.member "b" t),
+  "Contains 'ab'"        ~: True  ~=? (Anatree.member "ab" t),
+  "Contains 'ba'"        ~: True  ~=? (Anatree.member "ba" t)
   ]
 
 -- hUnitTestToTests: Adapt an existing HUnit test into a list of test-framework tests
 tests :: [Test.Framework.Test]
-tests = hUnitTestToTests $ TestList [ test_emptyInfo ]
+tests = hUnitTestToTests $ TestList [
+  tests_Empty,
+  tests_Eta,
+  tests_A, tests_A_A,
+  tests_B,
+  tests_A_B, tests_B_A, tests_BA_AB, tests_AB_BA, tests_A_AB_A,
+  tests_AB_B, tests_BA_AB_B, tests_B_AB_B, tests_Eta_BA_A_AB_B
+  ]
 
 main :: IO ()
 main = defaultMain tests
